Fixed loss computation, added reconstruction and output log

compressive_transformer.py
@@ -55,10 +55,8 @@ class CompressiveEncoder(nn.Module):
         cmems = torch.stack([d_cmem, b_cmem, g_cmem, s_cmem])
         mems, cmems = map(torch.detach, (mems, cmems))
         latents = torch.stack([d_z, b_z, g_z, s_z], dim=1)
-        aux_loss = torch.stack((d_l, b_l, g_l, s_l))
-        aux_loss = torch.mean(aux_loss)
-        ae_loss = torch.stack((d_ae, b_ae, g_ae, s_ae))
-        ae_loss = torch.mean(ae_loss)
+        aux_loss = torch.stack((d_l, b_l, g_l, s_l)).mean()
+        ae_loss = torch.stack((d_ae, b_ae, g_ae, s_ae)).mean()
         return latents, mems, cmems, aux_loss, ae_loss
 
 
@@ -66,23 +64,26 @@ class LatentsCompressor(nn.Module):
     def __init__(self,
                  d_model=config["model"]["d_model"],
                  seq_len=config["model"]["seq_len"],
-                 n_latents=config["data"]["max_track_length"] // config["model"]["seq_len"],
+                 n_latents=config["model"]["total_seq_len"] // config["model"]["seq_len"],
                  z_i_dim=config["model"]["z_i_dim"],
                  z_tot_dim=config["model"]["z_tot_dim"]
                  ):
         super(LatentsCompressor, self).__init__()
-        assert config["data"]["max_track_length"] % config["model"]["seq_len"] == 0
         self.sequence_compressor = nn.Linear((seq_len+1)*d_model, z_i_dim)  # +1 because adding sos and eos
         self.track_compressor = nn.Linear(z_i_dim*n_latents, z_tot_dim)
         self.song_compressor = nn.Linear(z_tot_dim*4, z_tot_dim)
+        self.act = torch.nn.ReLU()
 
     def forward(self, latents):
         latents = torch.flatten(latents, start_dim=3)
         latents = self.sequence_compressor(latents)
+        # latents = self.act(latents)
         latents = torch.flatten(latents, start_dim=2)
         latents = self.track_compressor(latents)
+        # latents = self.act(latents)
         latents = torch.flatten(latents, start_dim=1)
         latents = self.song_compressor(latents)
+        # latents = self.act(latents)
         return latents
 
 
@@ -90,12 +91,11 @@ class LatentsDecompressor(nn.Module):
     def __init__(self,
                  d_model=config["model"]["d_model"],
                  seq_len=config["model"]["seq_len"],
-                 n_latents=config["data"]["max_track_length"] // config["model"]["seq_len"],
+                 n_latents=config["model"]["total_seq_len"] // config["model"]["seq_len"],
                  z_i_dim=config["model"]["z_i_dim"],
                  z_tot_dim=config["model"]["z_tot_dim"]
                  ):
         super(LatentsDecompressor, self).__init__()
-        assert config["data"]["max_track_length"] % config["model"]["seq_len"] == 0
         self.sequence_decompressor = nn.Linear(z_i_dim, (seq_len+1)*d_model)  # +1 because adding sos and eos
         self.track_decompressor = nn.Linear(z_tot_dim, z_i_dim*n_latents)
         self.song_decompressor = nn.Linear(z_tot_dim, z_tot_dim*4)
@@ -104,12 +104,16 @@ class LatentsDecompressor(nn.Module):
         self.n_latents = n_latents
         self.seq_len = seq_len
         self.d_model = d_model
+        self.act = torch.nn.ReLU()
 
     def forward(self, latents):  # 1 x 1024 -> 1 x 4 x 10 x 301 x 32
+        # latents = self.act(latents)
         latents = self.song_decompressor(latents)
         latents = latents.reshape(*latents.shape[:-1], 4, self.z_tot_dim)
+        # latents = self.act(latents)
         latents = self.track_decompressor(latents)
         latents = latents.reshape(*latents.shape[:-1], self.n_latents, self.z_i_dim)
+        # latents = self.act(latents)
         latents = self.sequence_decompressor(latents)
         latents = latents.reshape(*latents.shape[:-1], self.seq_len+1, self.d_model)
         return latents
@@ -186,7 +190,9 @@ class Encoder(nn.Module):
         seq = torch.cat((seq, pad), dim=-1)
         for b, s in enumerate(seq):  # add eos token
             idx = torch.nonzero(s == config["tokens"]["pad"])
-            seq[b][idx] = config["tokens"]["eos"]
+            if s.shape[0] == idx.shape[0]:
+                continue  # all pad, do nothing
+            seq[b][idx[0]] = config["tokens"]["eos"]
 
         attn_losses = torch.tensor(0., requires_grad=True, device=seq.device, dtype=torch.float32)
         ae_losses = torch.tensor(0., requires_grad=True, device=seq.device, dtype=torch.float32)
@@ -203,8 +209,6 @@ class Encoder(nn.Module):
             ae_losses = ae_losses + ae_loss
         mems = torch.stack(new_mem)
         cmems = torch.stack(new_cmem)
-        # seq = seq.reshape(n_batches, (seq_len + 1) * self.d_model)  # flat bar tokens
-        # seq = self.compress_bar(seq)  # compress bar tokens
         attn_loss = attn_losses / self.N  # normalize w.r.t number of layers
         ae_loss = ae_losses / self.N  # normalize w.r.t number of layers
         return seq, mems, cmems, attn_loss, ae_loss
@@ -228,13 +232,12 @@ class Decoder(nn.Module):
         sos = torch.empty((seq.shape[0], 1), dtype=torch.int64).fill_(config["tokens"]["sos"]
                                                                       ).to(config["train"]["device"])
         seq = torch.cat((sos, seq), dim=-1)
-        for b, s in enumerate(seq):  # add eos token
+        for b, s in enumerate(seq):  # add eos token if possible
             idx = torch.nonzero(s == config["tokens"]["pad"])
-            if idx.dim() > 0:
-                seq[b][idx] = config["tokens"]["eos"]
-        n_batches, seq_len = seq.shape
-        # latent = self.decompress_bar(latent)
-        # latent = latent.reshape(n_batches, self.seq_len - 1, self.d_model)
+            if idx.shape[0] > 0:
+                if idx.shape[0] == s.shape[0]:  # all pad token
+                    continue
+                seq[b][idx[0]] = config["tokens"]["eos"]
         attn_losses = torch.tensor(0., requires_grad=True, device=seq.device, dtype=torch.float32)
         ae_losses = torch.tensor(0., requires_grad=True, device=seq.device, dtype=torch.float32)
         input_mask = seq != self.pad_token
@@ -287,7 +290,7 @@ class DecoderLayer(nn.Module):
 
 class MemoryMultiHeadedAttention(nn.Module):
     def __init__(self, h, dim, seq_len, mem_len, cmem_len, cmem_ratio, dropout=0.1, attn_dropout=0.1,
-                 reconstruction_attn_dropout=0.1, mask_subsequent=None):
+                 reconstruction_attn_dropout=0.1, ae_dropout=0.1, mask_subsequent=None):
         super(MemoryMultiHeadedAttention, self).__init__()
         assert dim % h == 0
         # We assume d_v always equals d_k
@@ -309,6 +312,7 @@ class MemoryMultiHeadedAttention(nn.Module):
         self.to_kv = nn.Linear(dim, kv_dim * 2, bias=False)
         self.to_out = nn.Linear(dim, dim)
         self.attn_dropout = nn.Dropout(attn_dropout)
+        self.ae_dropout = nn.Dropout(ae_dropout)
         self.dropout = nn.Dropout(dropout)
         self.reconstruction_attn_dropout = nn.Dropout(reconstruction_attn_dropout)
         self.deconv = nn.Linear(cmem_len, mem_len)
@@ -340,23 +344,15 @@ class MemoryMultiHeadedAttention(nn.Module):
             mask = input_mask[:, None, :, None] * input_mask[:, None, None, :]
             mask = F.pad(mask, (mem_len + cmem_len, 0), value=True)
             dots.masked_fill_(~mask, mask_value)  # dots has values in the upper square, the rest is -inf
-
+        # here we add +1 to have a pattern t, f, f -> t, t, f -> t, t, t (without memory, which are always t)
         if self.mask_subsequent:
             total_mem_len = mem_len + cmem_len
-            mask = torch.ones(t, t + total_mem_len, **to(x)).triu_(diagonal=1 + total_mem_len).bool()
+            mask = torch.ones(t, t + total_mem_len, **to(x)).triu_(diagonal=total_mem_len+1).bool()  # TODO +1 or not????
             dots.masked_fill_(mask[None, None, ...], mask_value)  # init with all memories and just first of sequence
 
         attn = dots.softmax(dim=-1)
         attn = self.attn_dropout(attn)
-
-        # if not self.training:
-        #     if self.attn_imgs < 10:
-        #         att_image = numpy.clip(attn[0][0].data.cpu().numpy(), 0, 1)
-        #         wandb.log({'heatmap_with_text': wandb.plots.HeatMap(["x"] * 100, ["y"]*100, att_image[:100, :100], show_text=False)})
-        #         self.attn_imgs += 1
-        # else:
-        #     self.attn_imgs = 0
-
+        # TODO add here way to see where attention is focusing
         out = torch.einsum('bhij,bhjd->bhid', attn, v)
         out = out.transpose(1, 2).reshape(b, t, -1)
         logits = self.to_out(out)
@@ -368,8 +364,8 @@ class MemoryMultiHeadedAttention(nn.Module):
         ae_loss = torch.zeros(1, requires_grad=True, **to(q))
 
         # if seq_len > t means that the sequence is over, so no more memory update is needed
-        # if self.seq_len > t or not calc_memory:  # TODO removed, we never enter here because t = self.seq_len
-        # return logits, Memory(new_mem, new_cmem), aux_loss
+        if self.seq_len > t or not calc_memory:  # TODO removed, we never enter here because t = self.seq_len
+            return logits, Memory(new_mem, new_cmem), aux_loss, ae_loss
 
         # calculate memory and compressed memory
 
@@ -413,6 +409,7 @@ class MemoryMultiHeadedAttention(nn.Module):
         to_pad = self.cmem_len - new_cmem.shape[1]
         new_cmem = F.pad(new_cmem, (0, 0, to_pad, 0), value=0.)
         reconstructed_mem = self.deconv(new_cmem.transpose(1, 2)).transpose(1, 2)
+        reconstructed_mem = self.ae_dropout(reconstructed_mem)
         to_pad = self.mem_len - old_mem.shape[1]
         old_mem = F.pad(old_mem, (0, 0, to_pad, 0), value=0.)
 

iterate_dataset.py
@@ -6,7 +6,7 @@ from torch.utils.data import SubsetRandomSampler
 
 
 class SongIterator(torch.utils.data.Dataset):
-    def __init__(self, dataset_path, test_size, batch_size=3, n_workers=1):
+    def __init__(self, dataset_path, test_size, max_len=3000, batch_size=3, n_workers=1):
         self.dataset_path = dataset_path
         _, _, songs = next(os.walk(self.dataset_path))
         self.songs = [x.split(".")[0] for x in songs]
@@ -15,6 +15,7 @@ class SongIterator(torch.utils.data.Dataset):
         self.ts_set = self.songs[:ts_length]
         self.tr_set = self.songs[ts_length:]
         self.batch_size = batch_size
+        self.max_len = max_len
         if len(self.tr_set) < batch_size:
             raise Exception("Training set is too little w.r.t. the batch size")
         if len(self.ts_set) < batch_size:
@@ -24,7 +25,7 @@ class SongIterator(torch.utils.data.Dataset):
     def __getitem__(self, idx):
         with open(os.path.join(self.dataset_path, idx+str('.pickle')), 'rb') as file:
             song = pickle.load(file)
-        return song
+        return song[:, :self.max_len]
 
     def __len__(self, train=None):
         if train:

main.py
@@ -17,11 +17,6 @@ import os
 # TO DETACH ctrl+b d
 # TO VISUALIZE GPUs STATUS: nvidia-smi
 # TO GET RESULTS: scp -r berti@131.114.137.168:MusAE/2020* C:\Users\berti\PycharmProjects\MusAE\remote_results
-# def train_aae():
-#     set_freer_gpu()
-#     ct = torch.load(os.path.join("remote_results", "checkpoint_16.pt"))
-#     trainer = AAETrainer(model=ct, dataset_path=config["paths"]["dataset_path"], **config["train"], config=config)
-#     trainer.train()
 
 
 def train_ct():
@@ -37,6 +32,6 @@ def train_ct():
 
 
 if __name__ == "__main__":  # TODO put here assert I guess
-    assert config["data"]["max_track_length"] % config["model"]["seq_len"] == 0, 'adjust max_track_length seq_len'
+    assert config["model"]["total_seq_len"] % config["model"]["seq_len"] == 0, 'adjust total_seq_len seq_len'
     train_ct()
     # train_aae()

optimizer.py
@@ -1,24 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from config import config
+
+
 class CTOpt:
     """
     Optimizer wrapper that implements rate.
     """
 
-    def __init__(self, optimizer, warmup_steps, warmup_interval):
+    def __init__(self, optimizer, warmup_steps, warmup_interval, decay_steps, minimum):
         self.optimizer = optimizer
         self.step = 0
         self.warmup_steps = warmup_steps
         self.warmup_interval = warmup_interval
+        self.decay_steps = decay_steps
+        self.minimum = minimum
+        self.lr = 0
 
     def zero_grad(self):
         self.optimizer.zero_grad()
 
-    def optimize(self):
+    def get_lr(self):
         if self.step < self.warmup_steps:
             lr = (self.warmup_interval[1] - self.warmup_interval[0]) * (self.step / self.warmup_steps)\
                  + self.warmup_interval[0]
+        elif self.step < self.warmup_steps + self.decay_steps:
+            lr = self.warmup_interval[1]*((np.cos(((self.step-self.warmup_steps)*np.pi)/self.decay_steps)+1)/2)+self.minimum
         else:
-            lr = self.warmup_interval[1]
-        for p in self.optimizer.param_groups:
-            p['lr'] = lr
+            lr = self.minimum
         self.step += 1
+        return lr
+
+    def optimize(self):
+        self.lr = self.get_lr()
+        for p in self.optimizer.param_groups:
+            p['lr'] = self.lr
         self.optimizer.step()
+
+
+if __name__ == "__main__":
+    opt = CTOpt(None, config["train"]["warmup_steps"],
+                (config["train"]["lr_min"], config["train"]["lr_max"]),
+                config["train"]["decay_steps"],
+                config["train"]["minimum_lr"])
+    x = list(range(config["train"]["warmup_steps"]*2+config["train"]["decay_steps"]))
+    y = []
+    for xi in x:
+        y.append(opt.get_lr())
+    plt.plot(x, y)
+    plt.show()

train.py
@@ -24,6 +24,7 @@ class Trainer:
         self.plot_name = plot_name
         self.settings = settings
         self.epoch = 0
+        self.step = 0
         self.loss_computer = None
         self.optimizer = None
         # Models
@@ -72,30 +73,39 @@ class Trainer:
         latents = self.decompressor(latents)
         latents = latents.transpose(0, 2)
         for seq, latent in zip(src, latents):
+            if not self.decoder.training:  # do not use teacher forcing
+                seq = torch.empty(4, config["train"]["batch_size"], 0, dtype=torch.long).to(config["train"]["device"])
+                for _ in range(config["model"]["seq_len"]):
+                    out, _, _, _, _ = self.decoder(latent, seq, d_mems, d_cmems)
+                    out = torch.max(out, dim=-2).indices
+                    out = out.transpose(1, 2).transpose(0, 1)
+                    seq = torch.cat((seq, out[..., -1:]), dim=-1)
             out, d_mems, d_cmems, d_attn_loss, d_ae_loss = self.decoder(latent, seq, d_mems, d_cmems)
-            d_attn_losses.append(d_attn_loss)  # TODO fix loss compute
+            d_attn_losses.append(d_attn_loss)
             d_ae_losses.append(d_ae_loss)
-            outs.append(out[:, :-1, :, :])
+            outs.append(out[:, :-1, :, :])  # TODO ALUA????
         outs = torch.stack(outs, dim=1)
         outs = outs.reshape(config["train"]["batch_size"], -1, config["tokens"]["vocab_size"], 4)
         e_ae_losses = torch.stack(e_ae_losses).mean()
         e_attn_losses = torch.stack(e_attn_losses).mean()
         d_ae_losses = torch.stack(d_ae_losses).mean()
         d_attn_losses = torch.stack(d_attn_losses).mean()
-        # n_tokens = torch.count_nonzero(src)
-        # n_tokens_drums = torch.count_nonzero(src[..., 0])
-        # n_tokens_bass = torch.count_nonzero(src[..., 1])
-        # n_tokens_guitar = torch.count_nonzero(src[..., 2])
-        # n_tokens_strings = torch.count_nonzero(src[..., 3])
-        # norm = (n_tokens, n_tokens_drums, n_tokens_bass, n_tokens_guitar, n_tokens_strings)
-        # bars = bars.transpose(1, 2)
-        # bars = bars.transpose(2, 3)
-        # bars = bars[:, :, 1:, :]  # 1, 5010, 4
         src = src.transpose(1, 3)
         src = src.transpose(0, 2)
         src = src.transpose(1, 2)
         src = src.reshape(config["train"]["batch_size"], -1, 4)
-        loss, loss_items = self.loss_computer(outs, src)  # , norm)  # TODO pad token and model output...?
+
+        # TODO log example of src and outs sometimes
+        if self.step % 100 == 0:
+            predicted = torch.max(outs, dim=-2).indices
+            inp = src[0].cpu().transpose(0, 1).numpy()
+            x = predicted[0].cpu().transpose(0, 1).numpy()
+            y = src[0].cpu().transpose(0, 1).cpu().numpy()
+            table = wandb.Table(columns=["Input", "Predicted", "Expected"])
+            table.add_data(inp, x, y)
+            wandb.log({"out_"+str(self.step): table})
+
+        loss, loss_items = self.loss_computer(outs, src)
         if self.encoder.training:
             self.model_optimizer.zero_grad()
             (loss + e_attn_losses + e_ae_losses + d_attn_losses + d_ae_losses).backward()
@@ -155,7 +165,8 @@ class Trainer:
 
     def log_to_wandb(self, losses):
         mode = "train/" if self.encoder.training else "eval/"
-        wandb.log({mode + "loss": losses[0],
+        wandb.log({"stuff/lr": self.model_optimizer.lr,
+                   mode + "loss": losses[0],
                    mode + "encoder attention loss": losses[1],
                    mode + "encoder autoencoder loss": losses[2],
                    mode + "decoder attention loss": losses[3],
@@ -168,32 +179,40 @@ class Trainer:
         # mode + "discriminator loss": losses[10],
         # mode + "generator loss": losses[11]})
 
-    def reconstruct(self, model, src, note_manager=None):
-        # src = src.reshape(config["train"]["batch_size"], 4, self.n_latents, config["model"]["seq_len"])
-        # src = src.transpose(0, 2)  # swap batches and sequences
-        # src = torch.LongTensor(src.long()).to(config["train"]["device"])  # but is alread a tensor
-        # e_mems, e_cmems, d_mems, d_cmems = self.get_memories()
-        # outs = []
-        # for seq in src:
-        #
-        #     latent, e_mems, e_cmems, _, _ = model.encode(seq, e_mems, e_cmems)
-        #     out, d_mems, d_cmems, _, _ = model.decode(latent, seq, d_mems, d_cmems)
-        #     out = torch.max(out, dim=-2).indices
-        #     outs.append(out)
-        # outs = torch.stack(outs)
-        # src = src.cpu()
-        # outs = outs.cpu()
-        # outs = outs.transpose(0, 1)  # invert bar and batch
-        # outs = outs.transpose(0, -1)  # invert batch and instruments
-        # outs = outs[:, :, :, 0]  # take first song of batch
-        # src = src.transpose(0, 1)
-        # src = src.transpose(-2, -1)
-        # src = src[:, :, :, 0]
-        # original = note_manager.reconstruct_music(src.reshape(4, -1).numpy())
-        # reconstructed = note_manager.reconstruct_music(outs.reshape(4, -1).numpy())  # flat bars
-        # reconstructed = note_manager.cut_song(reconstructed, original.get_end_time())  # cut reconstructed song length
-        # return original, reconstructed
-        pass
+    def reconstruct(self, src, note_manager):
+        src = src.reshape(*src.shape[:-1], -1, config["model"]["seq_len"])
+        src = src.transpose(0, 2)  # swap batches and sequences
+        src = torch.LongTensor(src.long()).to(config["train"]["device"])
+        e_mems, e_cmems, d_mems, d_cmems = self.get_memories()
+        outs = []
+        latents = []
+        for seq in src:
+            latent, e_mems, e_cmems, _, _ = self.encoder(seq, e_mems, e_cmems)
+            latents.append(latent)
+        latents = torch.stack(latents, dim=2)  # 1 x 4 x 10 x 301 x 32
+        latents = self.compressor(latents)  # 1 x z_dim
+        latents = self.decompressor(latents)
+        latents = latents.transpose(0, 2)
+        for latent in latents:
+            seed = torch.empty(4, config["train"]["batch_size"], 0, dtype=torch.long).to(config["train"]["device"])
+            for _ in range(config["model"]["seq_len"]):
+                out, _, _, _, _ = self.decoder(latent, seed, d_mems, d_cmems)
+                out = torch.max(out, dim=-2).indices
+                out = out.transpose(1, 2).transpose(0, 1)
+                seed = torch.cat((seed, out[..., -1:]), dim=-1)
+            out, d_mems, d_cmems, _, _ = self.decoder(latent, seed, d_mems, d_cmems)
+            out = torch.max(out, dim=-2).indices
+            out = out.transpose(1, 2)
+            outs.append(out[..., 1:])
+        outs = torch.stack(outs, dim=2)
+        outs = outs.reshape(config["train"]["batch_size"], 4, -1)
+        src = src.transpose(0, 2)
+        src = src.reshape(*src.shape[:-2], -1)
+        src = src[0]
+        outs = outs[0]
+        original = note_manager.reconstruct_music(src.cpu().numpy())
+        reconstructed = note_manager.reconstruct_music(outs.cpu().numpy())
+        return original, reconstructed
 
     def generate(self, iterations=10):
         # z = Variable(torch.randn((self.batch_size, self.z_dim))).cuda()
@@ -253,7 +272,9 @@ class Trainer:
                                                        {"params": self.decompressor.parameters()},
                                                        {"params": self.decoder.parameters()}], lr=0),
                                      config["train"]["warmup_steps"],
-                                     (config["train"]["lr_min"], config["train"]["lr_max"]))  # TODO add other
+                                     (config["train"]["lr_min"], config["train"]["lr_max"]),
+                                     config["train"]["decay_steps"], config["train"]["minimum_lr"])  # TODO add other
+
         # self.optim_discriminator = torch.optim.Adam(self.discriminator.parameters(), lr=1e-4)
         # self.optim_generator = torch.optim.Adam(self.model.encoder.parameters(), lr=5e-5)
         # Loss
@@ -266,6 +287,7 @@ class Trainer:
         # Dataset
         dataset = SongIterator(dataset_path=config["paths"]["dataset"],
                                test_size=config["train"]["test_size"],
+                               max_len=config["model"]["total_seq_len"],
                                batch_size=config["train"]["batch_size"],
                                n_workers=config["train"]["n_workers"])
         tr_loader, ts_loader = dataset.get_loaders()
@@ -285,6 +307,7 @@ class Trainer:
         train_progress = tqdm(total=config["train"]["mb_before_eval"], position=0, leave=True, desc=desc)
         trained = False
         it_counter = 0
+        self.step = 0
         best_ts_loss = float("inf")
         # best_aae_disc_loss = float("inf")
         # best_aae_gen_loss = float("inf")
@@ -302,7 +325,6 @@ class Trainer:
                     for test in tqdm(ts_loader, position=0, leave=True, desc=desc):  # remember test losses
                         ts_loss = self.run_mb(test)
                         ts_losses.append(ts_loss)
-                        break  # TODO remove, added just for debugging
                     final = ()  # average losses
                     for i in range(len(ts_losses[0])):  # for each loss value
                         aux = []
@@ -310,19 +332,6 @@ class Trainer:
                             aux.append(loss[i])
                         avg = sum(aux) / len(aux)
                         final = final + (avg,)
-                    # if final[0] < 1:  # song are well reconstructed, we can check the aae model
-                    #     if final[9] < best_aae_recon_loss and final[10] < best_aae_disc_loss and \
-                    #             final[11] < best_aae_gen_loss:
-                    #         print("Saving best aae model in " + os.path.join(self.save_path, "aae"))
-                    #         for filename in glob.glob(os.path.join(self.save_path, "aae" + '*')):
-                    #             os.remove(filename)
-                    #         best_aae_recon_loss = final[9]
-                    #         best_aae_disc_loss = final[10]
-                    #         best_aae_gen_loss = final[11]
-                    #         torch.save(self.Q_net, "aae_encoder")
-                    #         torch.save(self.P_net, "aae_decoder")
-                    #         torch.save(self.D_net_gauss, "aae_discriminator")
-                    #         print("aae model saved")
                     if final[0] < best_ts_loss:
                         new_model = os.path.join(self.save_path, self.model_name + '_' + str(self.epoch) + '.pt')
                         print("Saving best model in " + new_model + ", DO NOT INTERRUPT")
@@ -330,36 +339,34 @@ class Trainer:
                             os.remove(filename)
                         best_ts_loss = final[0]
                         torch.save(self.encoder, os.path.join(self.save_path, "encoder_" + str(self.epoch) + '.pt'))
-                        torch.save(self.compressor, os.path.join(self.save_path, "encoder_" + str(self.epoch) + '.pt'))
-                        torch.save(self.decompressor, os.path.join(self.save_path, "encoder_" + str(self.epoch) + '.pt'))
-                        torch.save(self.decoder, os.path.join(self.save_path, "encoder_" + str(self.epoch) + '.pt'))
+                        torch.save(self.compressor, os.path.join(self.save_path, "compressor_" + str(self.epoch)+'.pt'))
+                        torch.save(self.decompressor, os.path.join(self.save_path, "decompressor_"+str(self.epoch)+'.pt'))
+                        torch.save(self.decoder, os.path.join(self.save_path, "decoder_" + str(self.epoch) + '.pt'))
                         print("Model saved")
                     self.log_to_wandb(final)
                     # TODO reconstruction and generation
                     # # generated = self.generate(iterations=config["train"]["generated_iterations"])  # generation
-                    # note_manager = NoteRepresentationManager()
-                    # original, reconstructed = self.reconstruct(self.model, test, note_manager)
-                    # prefix = "epoch_" + str(self.epoch) + "_mb_" + str(it)
-                    # original.write_midi(os.path.join(wandb.run.dir, prefix + "_original.mid"))
-                    # reconstructed.write_midi(os.path.join(wandb.run.dir, prefix + "_reconstructed.mid"))
-                    # # generated.write_midi(os.path.join(wandb.run.dir, prefix+"_generated.mid"))
-                    # midi_to_wav(os.path.join(wandb.run.dir, prefix + "_original.mid"),
-                    #             os.path.join(wandb.run.dir, prefix + "_original.wav"))
-                    # midi_to_wav(os.path.join(wandb.run.dir, prefix + "_reconstructed.mid"),
-                    #             os.path.join(wandb.run.dir, prefix + "_reconstructed.wav"))
+                    note_manager = NoteRepresentationManager()
+                    original, reconstructed = self.reconstruct(test, note_manager)
+                    prefix = "epoch_" + str(self.epoch) + "_mb_" + str(it)
+                    original.write_midi(os.path.join(wandb.run.dir, prefix + "_original.mid"))
+                    reconstructed.write_midi(os.path.join(wandb.run.dir, prefix + "_reconstructed.mid"))
+                    # generated.write_midi(os.path.join(wandb.run.dir, prefix+"_generated.mid"))
+                    midi_to_wav(os.path.join(wandb.run.dir, prefix + "_original.mid"),
+                                os.path.join(wandb.run.dir, prefix + "_original.wav"))
+                    midi_to_wav(os.path.join(wandb.run.dir, prefix + "_reconstructed.mid"),
+                                os.path.join(wandb.run.dir, prefix + "_reconstructed.wav"))
                     # # midi_to_wav(os.path.join(wandb.run.dir, prefix + "_generated.mid"),
                     # #             os.path.join(wandb.run.dir, prefix + "_generated.wav"))
-                    # wandb.log({str(it_counter): [
-                    #     wandb.Audio(os.path.join(wandb.run.dir, prefix + "_original.wav"),
-                    #                 caption="original", sample_rate=32),
-                    #     wandb.Audio(os.path.join(wandb.run.dir, prefix + "_reconstructed.wav"),
-                    #                 caption="reconstructed", sample_rate=32),
-                    #     # wandb.Audio(os.path.join(wandb.run.dir, prefix + "_generated.wav"),
-                    #     #             caption="generated", sample_rate=32)
-                    # ]})
+                    wandb.log({str(it_counter): [
+                        wandb.Audio(os.path.join(wandb.run.dir, prefix + "_original.wav"),
+                                    caption="original", sample_rate=32),
+                        wandb.Audio(os.path.join(wandb.run.dir, prefix + "_reconstructed.wav"),
+                                    caption="reconstructed", sample_rate=32),
+                        #      wandb.Audio(os.path.join(wandb.run.dir, prefix + "_generated.wav"),
+                        #                  caption="generated", sample_rate=32)
+                    ]})
                     it_counter += 1
-                    # wandb.save(os.path.join(wandb.run.dir, prefix + "_original.mid"))
-                    # wandb.save(os.path.join(wandb.run.dir, prefix + "_reconstructed.mid"))
                     self.encoder.train()
                     self.compressor.train()
                     self.decompressor.train()
@@ -370,3 +377,4 @@ class Trainer:
                 self.log_to_wandb(tr_losses)
                 train_progress.update()
                 trained = True
+                self.step += 1